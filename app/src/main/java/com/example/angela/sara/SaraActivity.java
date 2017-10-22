ab517cb6a6c0b381f426ea8aaf08fac512871942
Angela Londono <l.angela05@gmail.com>
2017-10-22 18:27:35 -0500

    first commit

---
+++ app/src/main/java/com/example/angela/sara/SaraActivity.java
@@ -0,0 +1,13 @@
+package com.example.angela.sara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sara);
+    }
+}
